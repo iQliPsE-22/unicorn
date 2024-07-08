8d066b72eecb71ee8952eb1302a73ee2f7aefeee
Deepak Kumar <deepaklg02@gmail.com>
2024-07-08 17:57:23 +0530

    added folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import Hero from "./component/Hero";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Hero from './component/hero/Hero';
 const App = () => {
   return (
     <div>
